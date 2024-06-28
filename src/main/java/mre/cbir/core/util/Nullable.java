3598d0ccda19e7f5a41755cf31be31f09ef91fcb
michael-re <102764781+michael-re@users.noreply.github.com>
2024-06-27 21:44:56 -0700

    feat: create interface to retrieve nullable values

---
+++ src/main/java/mre/cbir/core/util/Nullable.java
@@ -0,0 +1,18 @@
+package mre.cbir.core.util;
+
+public interface Nullable<T>
+{
+    T get() throws Exception;
+
+    static <T> T value(final Nullable<T> value)
+    {
+        try
+        {
+            return value.get();
+        }
+        catch (final Exception e)
+        {
+            return null;
+        }
+    }
+}
